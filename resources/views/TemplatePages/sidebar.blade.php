e0471fb1490712c65e32942a9dda36defd4fb5ac
gazala1234 <ustadgazal@gmail.com>
2024-08-10 12:58:36 +0530

    added conditions to show items based on the role

--- resources/views/TemplatePages/sidebar.blade.php
+++ resources/views/TemplatePages/sidebar.blade.php
@@ -9,25 +9,64 @@
                 </a>
             </li><!-- End Dashboard Nav -->
 
-            <li class="nav-item">
-                <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
-                    href="#">
-                    <i class="bi bi-folder-plus" style="font-size: 20px;"></i><span>Assignments</span><i
-                        class="bi bi-chevron-down ms-auto"></i>
-                </a>
-                <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                    <li>
-                        <a href="{{ route('add_assignment') }}">
-                            <i class="bi bi-plus-circle-fill" style="font-size: 13px;"></i><span>Add Assignment</span>
+            {{-- TECHING AND NON-TEACHING SIDEBAR --}}
+            @php
+                $role = session('role');
+                // echo $role;
+                if($role == 'Teaching' || $role == 'Non-Teaching'){
+            @endphp
+                    <li class="nav-item">
+                        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
+                            href="#">
+                            <i class="bi bi-folder-plus" style="font-size: 20px;"></i><span>Assignments</span><i
+                                class="bi bi-chevron-down ms-auto"></i>
                         </a>
+                        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                            <li>
+                                <a href="{{ route('add_assignment') }}">
+                                    <i class="bi bi-plus-circle-fill" style="font-size: 13px;"></i><span>Add Assignment</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('view_assignment') }}">
+                                    <i class="bi bi-table" style="font-size: 13px;"></i><span>View Assignment</span>
+                                </a>
+                            </li>
+                        </ul>
                     </li>
-                    <li>
-                        <a href="{{ route('view_assignment') }}">
-                            <i class="bi bi-table" style="font-size: 13px;"></i><span>View Assignment</span>
-                        </a>
-                    </li>
-                </ul>
-            </li><!-- End Assignments Nav -->
+            @php
+                }
+            @endphp
+
+            {{-- ADMIN SIDEBAR --}}
+            @php
+            $role = session('role');
+            // echo $role;
+            if($role == 'admin'){
+            @endphp
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="{{ route('contact') }}">
+                        <i class="bi bi-table" style="font-size: 20px;"></i><span>Assigned Tasks</span>                   
+                    </a>
+                </li>
+            @php
+            }
+            @endphp
+
+            {{-- DEVELOPER SIDEBAR --}}
+            @php
+            $role = session('role');
+            // echo $role;
+            if($role == 'developer'){
+            @endphp
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="{{ route('contact') }}">
+                        <i class="bi bi-table" style="font-size: 20px;"></i><span>Assigned Tasks</span>                   
+                    </a>
+                </li>
+            @php
+            }
+            @endphp
 
             <li class="nav-item">
                 <a class="nav-link collapsed" href="{{ route('contact') }}">
